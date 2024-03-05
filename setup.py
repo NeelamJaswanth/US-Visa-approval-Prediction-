5ca6ce08ae65a61ba02129e8e10ff438be579d62
Neelam Jaswanth <neeljaswanth18@gmail.com>
2024-03-05 22:53:37 +0530

    requirements.txt added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    versio="0.0.0",
+    author = "Jaswanth",
+    author_email = "neeljaswanth18@gmail.com",
+    packages=find_packages()
+    )
